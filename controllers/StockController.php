01ceb487e77e8bdbe58676da0fb64048b5595e87
a-ludachris <parfaitalliance@gmail.com>
2024-08-30 15:12:57 +0100

    Méthode qui crée de fausses données de catégories et les liste

--- controllers/StockController.php
+++ controllers/StockController.php
@@ -1,2 +1,20 @@
 <?php
+
+// On charge tous nos éléments comme si on était dans index.php
+require_once "./../models/CategorieModel.php";
+
+class StockController{
+
+    public function listerCategories() : array {
+        $categories = [];
+        for ($i=1; $i <= 5; $i++) { 
+            $categorie = new CategorieModel();
+            $categorie->setId($i);
+            $categorie->setLibelle("Categorie".$i);
+        }
+        return $categories;
+    }
+
+}
+
 ?>
