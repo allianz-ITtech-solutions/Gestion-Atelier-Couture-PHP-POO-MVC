a56573acbf168759cedab6b5c4ff7f68deeeb34e
a-ludachris <parfaitalliance@gmail.com>
2024-08-30 18:14:43 +0100

    Met à jour la classe ArticleModel pour refléter les nouveaux changements

--- models/ArticleModel.php
+++ models/ArticleModel.php
@@ -1,6 +1,7 @@
 <?php
 
-class ArticleModel{
+// Comme on ne peut créer que des articles de vente et des articles de confection, la classe Article sera en 'abstract'
+abstract class ArticleModel{
 
     protected int $id;
     protected string $libelle;
@@ -10,6 +11,75 @@ class ArticleModel{
     // En PHP, les énumérations ne sont pas évolués. On le crée donc en string notre énumération type
     protected string $type;
     
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+
+    public function getQteStock()
+    {
+        return $this->qteStock;
+    }
+
+
+    public function setQteStock($qteStock)
+    {
+        $this->qteStock = $qteStock;
+
+        return $this;
+    }
+
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
 }
 
 ?>
