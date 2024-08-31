3afbf10da82914fbb9bb84c30181d9e7004d215c
a-ludachris <parfaitalliance@gmail.com>
2024-08-31 13:50:15 +0100

    Met à jour index avec un switch pour tester les requetes url avec GET

--- public/index.php
+++ public/index.php
@@ -2,9 +2,24 @@
 
 require_once "./../controllers/StockController.php";
 
-$stockCtrl = new StockController();
+$stockCtrl = new StockController;
 
-// Affiche la vue et liste les catégories 
-$stockCtrl->listerCategories();
+
+// On teste l'url sur la clé 'page' pour savoir sur quel lien on a cliqué
+switch ($_GET['page']) {
+    case 'categorie':
+        // Affiche la vue et liste les catégories
+        $stockCtrl->listerCategories();
+        break;
+
+    case 'article':
+        // Affiche la vue et liste les articles
+        $stockCtrl->listerArticles();
+        break;
+    
+    default:
+        # code...
+        break;
+}
 
 ?>
