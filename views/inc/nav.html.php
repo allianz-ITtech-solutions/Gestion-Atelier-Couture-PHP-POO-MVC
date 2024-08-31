671fac4d9e11828bb716097a261e492a13a29c47
a-ludachris <parfaitalliance@gmail.com>
2024-08-31 13:20:26 +0100

    Crée une navbar HTML

---
+++ views/inc/nav.html.php
@@ -0,0 +1,17 @@
+<div class="container">
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Gestion Atelier de Couture</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                    aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarID">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Categorie</a>
+                        <a class="nav-link" aria-current="page" href="#">Article</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+</div>
