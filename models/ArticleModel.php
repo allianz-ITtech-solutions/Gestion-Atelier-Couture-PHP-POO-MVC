9ddf1370183e67f1b864786ba76b2fe5bcb0bea3
a-ludachris <parfaitalliance@gmail.com>
2024-09-01 00:21:12 +0100

    Met à jour la classe Article avec héritage sur la classe Model

--- models/ArticleModel.php
+++ models/ArticleModel.php
@@ -1,7 +1,16 @@
 <?php
 
+require_once "./../models/Model.php";
+
+
 // Comme on ne peut créer que des articles de vente et des articles de confection, la classe Article sera en 'abstract'
-abstract class ArticleModel{
+/*
+    En BD, on représentera l'héritage dans le mode SINGLE_TABLE.
+    SINGLE_TABLE signifie que tous les attributs des classes issues 
+    de l'héritage (Classe mère et classes filles) seront regroupés dans une seule table et les 
+    attributs des classes filles seront nullables.
+*/
+abstract class ArticleModel extends Model{
 
     protected int $id;
     protected string $libelle;
@@ -10,7 +19,15 @@ abstract class ArticleModel{
 
     // En PHP, les énumérations ne sont pas évolués. On le crée donc en string notre énumération type
     protected string $type;
+
     
+    public function __construct()
+    {
+        // On appelle le constructeur de Model qui elle se connecte à la BD
+        parent::__construct();
+        // On initialise le nom de la table à 'articles'
+        $this->tableName = "articles";
+    }
 
     public function getId()
     {
@@ -21,8 +38,6 @@ abstract class ArticleModel{
     public function setId($id)
     {
         $this->id = $id;
-
-        return $this;
     }
 
 
@@ -35,8 +50,6 @@ abstract class ArticleModel{
     public function setLibelle($libelle)
     {
         $this->libelle = $libelle;
-
-        return $this;
     }
 
 
@@ -74,6 +87,16 @@ abstract class ArticleModel{
     {
         $this->type = $type;
     }
+
+
+    public function insert():int {
+        // Comme l'id est auto-incrément, on le met à NULL
+        $sql = "INSERT INTO `$this->tableName` (`id`, `libelle`) VALUES (NULL, :libelle)"; // Requete préparée
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(["libelle"=>$this->libelle]);
+        return $stmt->rowCount(); // Retourne le nombre de lignes supprimé
+    }
+
     
 }
 
