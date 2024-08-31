1ed0e88cc5197de331bbf403b4fc10f5e0eac338
a-ludachris <parfaitalliance@gmail.com>
2024-08-31 23:00:27 +0100

    Met à jour le CategorieModel avec les méthodes select, insert, update et delete

--- models/CategorieModel.php
+++ models/CategorieModel.php
@@ -5,6 +5,7 @@ class CategorieModel{
 
     private int $id;
     private string $libelle;
+    private string $tableName = "categories";
 
     /*
         PDO est un composant natif de PHP permettant l'accès à une base de données.
@@ -62,26 +63,49 @@ class CategorieModel{
         return $this;
     }
 
+
+    // query ===> requete sans paramètres. Il retourne un statement
+    // Un statement n'est rien d'autre que la table (lignes et colonnes)
+
+    // Par défaut, 'fetchAll' nous retourne les résultats de deux manières (tableau simple et tableau associatif)
+    // On doit donc lui spécifier le type de retour de ces résultats retournés
+    // 'FETCH_NUM' pour dire que les données soient en numériques
+    // 'FETCH_ASSOC' pour dire que les données soient en associatif
+    // FETCH_CLASS pour dire que les données soient en objet suivi du nom de la classe (CategorieModel)
+
     // Méthode Accès aux Données
     public function findAll(): array {
-        $sql = "select * from categories order by id asc";
-        // Query permet de faire une requete simple. Il retourne un statement
-        // Un statement n'est rien d'autre que la table (lignes et colonnes)
+        $sql = "select * from $this->tableName order by id asc";
         $stmt = $this->pdo->query($sql);
-
-        // Par défaut, 'fetchAll' nous retourne les résultats de deux manières (tableau simple et tableau associatif)
-        // On doit donc lui spécifier le type de retour de ces résultats retournés
-        // 'FETCH_NUM' pour dire que les données soient en numériques
-        // 'FETCH_ASSOC' pour dire que les données soient en associatif
-        // FETCH_CLASS pour dire que les données soient en objet suivi du nom de la classe (CategorieModel)
         return $stmt->fetchAll(\PDO::FETCH_CLASS, self::class);
     }
 
-    public function findById():object {
-        $sql = "select * from categories";
-        // Query permet d'éxécuter une requete simple (sans params)
-        $obj = $this->pdo->query($sql);
-        return $obj;
+    // prepare ===> requete avec paramètres.
+    // $stmt = "select * from categories where id = $id"; ===> Interdit car expose les données (Sensible aux Injections SQL)
+    // La bonne méthode est d'utiliser des jokers dans la requete (?, :nom).
+
+    // NB : 'fetch' ne prend pas en paramètre le type de retour (fetchMode)
+    public function findById(int $id):self {
+        $sql = "select * from $this->tableName where id = :x"; // Requete préparée
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, self::class);
+        $stmt->execute(["x"=>$id]);
+        return $stmt->fetch(); // Retourne un objet sous forme de CategorieModel
+    }
+
+    public function insert():int {
+        // Comme l'id est auto-incrément, on le met à NULL
+        $sql = "INSERT INTO `$this->tableName` (`id`, `libelle`) VALUES (NULL, :libelle)"; // Requete préparée
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(["libelle"=>$this->libelle]);
+        return $stmt->rowCount(); // Retourne le nombre de lignes supprimé
+    }
+
+    public function delete(int $id):int {
+        $sql = "delete from $this->tableName where id = :x"; // Requete préparée
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(["x"=>$id]);
+        return $stmt->rowCount(); // Retourne le nombre de lignes supprimé
     }
 
 }
