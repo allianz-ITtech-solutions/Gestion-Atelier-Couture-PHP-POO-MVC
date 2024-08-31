5adda6cc7245b3aa32a6e49363458ec051738add
a-ludachris <parfaitalliance@gmail.com>
2024-08-31 13:10:10 +0100

    Corrige un bug dans la méthode listerArticles()

--- controllers/StockController.php
+++ controllers/StockController.php
@@ -2,6 +2,10 @@
 
 // On charge tous nos éléments comme si on était dans index.php
 require_once "./../models/CategorieModel.php";
+require_once "./../models/ArticleModel.php";
+require_once "./../models/ArticleConfModel.php";
+require_once "./../models/ArticleVenteModel.php";
+
 
 class StockController{
 
@@ -16,6 +20,34 @@ class StockController{
 
         // Puis on charge la vue des catégories (Response HTML+CSS)
         require_once "./../views/categorie/liste.html.php";
+    }
+
+    public function listerArticles() {
+        
+        $articles = [];
+        
+        for ($i=1; $i <= 10; $i++) {
+            if ($i%2 == 0) {
+                $article = new ArticleVenteModel;
+                $article->setId($i);
+                $article->setLibelle("Article Vente ".$i);
+                $article->setPrixAchat($i*2000.0);
+                $article->setQteStock($i*100);
+                $article->setDateProd("1$i-05-2023");                
+            }else{
+                $article = new ArticleConfModel;
+                $article->setId($i);
+                $article->setLibelle("Article Confection ".$i);
+                $article->setPrixAchat($i*500.0);
+                $article->setQteStock($i*200);
+                $article->setFournisseur("Fournisseur ".$i);
+            }
+            
+            $articles[] = $article;
+        }
+
+        // Puis on charge la vue des catégories (Response HTML+CSS)
+        require_once "./../views/article/liste.html.php";
     }
 
 
