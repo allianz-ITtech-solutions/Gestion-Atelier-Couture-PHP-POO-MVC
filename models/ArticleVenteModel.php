9ffe71ab84b044f80b09fb7fb15fdf3f2c8d2bb7
a-ludachris <parfaitalliance@gmail.com>
2024-09-05 14:29:06 +0100

    Met à jour le constructeur en appelant le constructeur du parent ArticleModel

--- models/ArticleVenteModel.php
+++ models/ArticleVenteModel.php
@@ -8,6 +8,7 @@ class ArticleVenteModel  extends ArticleModel{
 
     public function __construct()
     {
+        parent::__construct();
         $this->type = 'ArticleVente';
     }
     
@@ -20,6 +21,12 @@ class ArticleVenteModel  extends ArticleModel{
     public function setDateProd($dateProd)
     {
         $this->dateProd = $dateProd;
+    }
+
+    // On a redéfini la méthode insert en lui passant la date de production
+    // NB : En redéfinissant, on doit respecter le prototype de la méthode hérité de la classe mère 
+    public function insert($data=null):int {
+        return parent::insert($this->dateProd);
     }
 
 }
