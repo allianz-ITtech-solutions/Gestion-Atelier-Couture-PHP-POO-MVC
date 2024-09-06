37a07943a4a4bd8b0da72c1ca1a87d0f6591e406
a-ludachris <parfaitalliance@gmail.com>
2024-09-06 16:28:31 +0100

    Finalisation du formulaire d'ajout d'une categorie

--- views/categorie/liste.html.php
+++ views/categorie/liste.html.php
@@ -27,6 +27,44 @@
         <main>
             <div class="container mt-5">
                 <div class="card mt-2">
+                    <!--
+                        Lorsqu'on a un seul champ dans un formulaire, il faut éviter de créer un formulaire
+                        pour ca et le mettre directement dans la liste.
+
+                        On doit donner au formulaire une method POST.
+                        On doit donner au formulaire un 'action' qui signifie ou doit etre envoyé les données.
+                        Chez nous les données doivent etre envoyés au niveau de index.php == http://localhost:8000
+
+                        On doit aussi donner un 'name' au input qui représente la clé de notre requete (comme 'page')
+                    -->
+                    <form class="d-flex mt-2" style="margin-left:20px" method="POST" action="http://localhost:8000/">
+                        <div class="col-5">
+                            <div class="mb-2">
+                                <label for="" class="form-label ml-1">Libelle</label>
+                                <input type="text" name="libelle" id="" class="form-control" placeholder="" aria-describedby="helpId"/>
+                                <small id="helpId" class="text-danger mt-1">Help text</small>
+                            </div>
+                        </div>
+                        <div class="col-2" style="margin-left:20px; margin-top: 30px;">
+                            <button name="" id="" class="btn btn-primary" type="submit">Enregistrer</button>                            
+                        </div>
+                        <!--
+                            Quand on clique sur le bouton enregistrer, on doit savoir quel action du controller 
+                            appeler qui est ajouterCategorie().
+
+                            Pour ca on va ajouter un champ 'input'. On doit le créer à chaque fois que nous avons un formulaire.
+                            On doit lui donner comme name la clé de la request (page) et comme type hidden, car il sera un champ caché.
+
+                            Et lui donne comme value, l'action (la méthode) à appeler qu'on va appeler 'add_categorie'.
+                            En gros, on cache l'action 'add_categorie'
+
+                            Au final on retournera deux champs:
+                            'libelle' qui contient la donnée
+                            'page' qui contient l'action à éxécuter (add_categorie)
+                        -->
+                        <input name="page" type="hidden" value="add_categorie">
+                    </form>
+                    
                   <div class="card-body">
                     <h5 class="card-title">Liste des Catégories</h5>
                     <div class="table-responsive table-bordered">
