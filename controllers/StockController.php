5b50be1ee93416c4275ea9d91e781046d22605b8
a-ludachris <parfaitalliance@gmail.com>
2024-09-28 22:18:51 +0100

    Ajout des Validations et Session dans la méthode AjouterCategorie

--- controllers/StockController.php
+++ controllers/StockController.php
@@ -1,7 +1,9 @@
 <?php
 
 // On charge tous nos éléments comme si on était dans index.php
-require_once "./../models/Model.php";
+require_once "./../config/Model.php";
+require_once "./../config/Validator.php";
+require_once "./../config/Session.php";
 require_once "./../models/CategorieModel.php";
 require_once "./../models/ArticleModel.php";
 require_once "./../models/ArticleConfModel.php";
@@ -22,6 +24,8 @@ class StockController{
     {
         $this->categorieModel = new CategorieModel;
         $this->articleModel = new ArticleModel;
+        // On démarre la session (une seule fois) dans le constructeur car toutes les méthodes peuvent l'utiliser
+        Session::start();
     }
 
     
@@ -37,14 +41,41 @@ class StockController{
     public function ajouterCategorie(): void {
         // On récupère les données saisies dans le champ
         extract($_POST); // Crée une variable nommée $libelle avec comme valeur la donnée du champ
-        // On affecte le libelle à un objet categorie
-        $this->categorieModel->setLibelle($libelle);
-        // Et on l'insère en base de données
-        $this->categorieModel->insert();
+        
+        // Un tableau associatif qui contiendra les erreurs du aux exceptions de BD
+        $errors = [];
+
+        // Avant d'insérer, on valide la valeur de libelle
+        Validator::isEmpty($libelle, "libelle");
+        
+        if (Validator::validate()) {
+
+            // Essaie d'insérer...
+            try {
+                // On affecte le libelle à un objet categorie
+                $this->categorieModel->setLibelle($libelle);
+
+                // Et on l'insère en base de données
+                $this->categorieModel->insert();
+
+            } catch (\Throwable $th) {
+                // Si l'insertion ne se passe pas bien et produit une exception, on ajoute au tableau l'erreur
+                $errors['libelle'] = "$libelle existe deja";
+            }
+        }
+        else {
+            // Champ est vide
+            $errors = Validator::getErrors();
+        }
+        // NB : Le tableau 'errors n'est pas disponible dans la vue des catégories, ce qui est
+        //      normal vu qu'on a pas inclu la vue ici, or on en a besoin.
+        //      Pour y accéder dans la liste, on va donc utiliser les variables de sessions.
+        // Dès qu'on a des informations qui doivent persister d'une requete à une autre, on utilise les sessions
+        Session::set("errors", $errors); // On ajoute le tableau d'erreurs dans la session
 
         // Après avoir ajouter, on doit afficher la liste des catégories, on fait donc une redirection
         // NB : Quand on est sur une action qui n'affiche pas une vue (exple: lister), on fait une redirection
-        header("location:http://localhost:8000/?page=categorie");
+        header("location:".BASE_URL."?page=categorie");
     }
 
 
