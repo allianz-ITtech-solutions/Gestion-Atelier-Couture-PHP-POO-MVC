461ec32eea73378ba9cbd85df2fc7a9f920db4c9
a-ludachris <parfaitalliance@gmail.com>
2024-10-03 21:44:51 +0100

    Déplacement de la méthode findAll dans la mère ArticleModel
    Factorisation de la méthode findAll dans la classe mère ArticleModel et
    simplifier le code

--- models/ArticleVenteModel.php
+++ models/ArticleVenteModel.php
@@ -23,6 +23,7 @@ class ArticleVenteModel  extends ArticleModel{
         $this->dateProd = $dateProd;
     }
 
+
     // On a redéfini la méthode insert en lui passant la date de production
     // NB : En redéfinissant, on doit respecter le prototype de la méthode hérité de la classe mère 
     public function insert($data=null):int {
