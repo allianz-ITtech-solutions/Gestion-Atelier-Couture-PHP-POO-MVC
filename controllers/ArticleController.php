f7e193b34d209dffca550729523b831cac28da0f
a-ludachris <parfaitalliance@gmail.com>
2024-10-04 01:21:35 +0100

    Méthode qui permet de lister les articles de vente et les articles de confection
    Récupère les articles de confection et de vente et les transmet à la vue

---
+++ controllers/ArticleController.php
@@ -0,0 +1,60 @@
+<?php
+
+// On charge tous nos éléments comme si on était dans index.php
+require_once "./../models/ArticleModel.php";
+require_once "./../models/ArticleConfModel.php";
+require_once "./../models/ArticleVenteModel.php";
+require_once "./../models/CategorieModel.php";
+
+
+class ArticleController extends Controller {
+    
+    private ArticleConfModel $articleConfModel;
+    private ArticleVenteModel $articleVenteModel;
+    private CategorieModel $categorieModel;
+    private ArticleModel $articleModel;
+
+
+    // Et on instancie categorieModel et ArticleModel dans le constructeur
+    public function __construct()
+    {
+        // NB : Le constructeur du parent sera toujours la première instruction de l'enfant
+        // On appelle le constructeur du parent qui démarre la session (une seule fois) dans le constructeur car toutes les méthodes peuvent l'utiliser
+        parent::__construct();
+
+        $this->articleConfModel = new ArticleConfModel;
+        $this->articleVenteModel = new ArticleVenteModel;
+        $this->categorieModel = new CategorieModel;
+        $this->articleModel = new ArticleModel;
+    }
+
+
+    // Méthode qui permet de lister les Articles
+    public function index() {
+        
+        $articlesC = $this->articleConfModel->findAll();
+        $articlesV = $this->articleVenteModel->findAll();
+
+        // On doit fusionner $articlesC et $articlesV
+        $articles = array_merge($articlesC, $articlesV);
+
+        // On appelle la méthode render_view qui va charger le contenu de la liste dans notre layout
+        $this->renderView("article/liste.html.php", [
+            "articles"=>$articles
+        ]);
+    }
+
+
+    
+
+
+    // Méthode qui permet d'enregistrer un article
+    public function save() {
+        
+        $this->articleModel->insert();
+
+    }
+
+}
+
+?>
