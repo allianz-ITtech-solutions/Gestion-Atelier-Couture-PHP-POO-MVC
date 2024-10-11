5a5c3f14585ad5c71aca9855f5f323b332d8ad1d
a-ludachris <parfaitalliance@gmail.com>
2024-10-11 19:42:35 +0100

    Creation et utilisation d'un objet UserModel pour la gestion de l'authentification
    Créer un objet de type UserModel pour avoir accéder au méthodes de base
    de données pour l'authentification d'un utilisateur.

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,10 +1,16 @@
 <?php
 
 // On charge tous nos éléments comme si on était dans index.php
+require_once "./../models/UserModel.php";
+
+
+// On charge tous nos éléments comme si on était dans index.php
 
 
 // Controller qui gère les méthodes de connexion et authentification
 class AuthController extends Controller {
+
+    private UserModel $userModel;
 
 
     public function __construct()
@@ -12,20 +18,42 @@ class AuthController extends Controller {
         // NB : Le constructeur du parent sera toujours la première instruction de l'enfant
         // On appelle le constructeur du parent qui démarre la session (une seule fois) dans le constructeur car toutes les méthodes peuvent l'utiliser
         parent::__construct();
+        $this->userModel = new UserModel;
     }
 
 
     // Méthode qui affiche le formulaire de connexion
     public function showLoginForm()
     {
-        
+        // On change le layout de la page de connexion avec notre page 'connexion.layout.html.php'
+        $this->layout = "connexion";
+        $this->renderView("auth/login.html.php");
     }
 
 
     // Méthode qui gère la connexion
     public function login()
     {
-        
+        // Controle de saisie
+        Validator::isEmail($_POST['login'], 'login');
+        Validator::isEmpty($_POST['password'], 'password');
+        if (Validator::validate()) {
+            extract($_POST);
+            // On vérifie si'utilisateur existe en BD
+            $user = $this->userModel->findUserByLoginAndPassword($login, $password);
+
+            // Si user est null, ca veut dire que l'utilisateur saisi n'existe pas en BD
+            // On stocke l'erreur dans le validator
+            if (!$user) {
+                Validator::addError("error-connexion", "Login ou Mot de passe incorrect");
+            }else{
+                $this->redirect("categorie");
+            }
+        }
+        // Si on arrive ici, c'est que le formulaire n'est pas valide
+        // On stocke les erreurs du Validator dans le tableau de la session
+        Session::set('errors', Validator::getErrors());
+        $this->redirect("show-form-login"); // Et on reste sur la page de connexion
     }
 
 
